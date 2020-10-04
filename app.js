cd4dbf3d2392ea21c1ac1a1215c56c4e38fed8a0
Nikolaj Dalianis <slidebass@gmail.com>
2020-10-04 23:45:39 +0200

    add script tag to index.html and computerPlay function to app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // build rock paper scissors game
+
+// function that randomly returns rock, paper or scissors as the computer's pick
+function computerPlay() {
+	let random = Math.floor(Math.random() * 3);
+	if (random === 0) {
+		return 'rock';
+	} else if (random === 1) {
+		return 'paper';
+	} else {
+		return 'scissors';
+	}
+}

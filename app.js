9f82547c71967efe9f659573531d98076f126dee
Nikolaj Dalianis <slidebass@gmail.com>
2020-10-08 01:18:42 +0200

    add playRound and game functions to app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // build rock paper scissors game
 
-// function that randomly returns rock, paper or scissors as the computer's pick
+// variables to keep track of score
+let playerScore = 0;
+let computerScore = 0;
+
+// randomly returns rock, paper or scissors as the computer's pick
 function computerPlay() {
 	let random = Math.floor(Math.random() * 3);
 	if (random === 0) {
@@ -12,7 +16,7 @@ function computerPlay() {
 	}
 }
 
-// get input from user, if input is correct return input, else loop request until input's correct
+// get input from user, if input is correct return input, else loop request until input is correct
 function userPlay() {
 	let pick = prompt('Choose rock, paper or scissors').toLowerCase();
 	if (pick === 'rock' || pick === 'paper' || pick === 'scissors') {
@@ -30,3 +34,66 @@ function userPlay() {
 	}
 	return pick;
 }
+
+// play a round, find and return a winner
+function playRound(playerSelection, computerSelection) {
+	let message = '';
+
+	// helper functions for player or computer wins
+	function playerWin(announceWinner) {
+		message = announceWinner;
+		playerScore += 1;
+		return message;
+	}
+
+	function computerWin(announceWinner) {
+		message = announceWinner;
+		computerScore += 1;
+		return message;
+	}
+
+	if (playerSelection === computerSelection) {
+		message = "It's a tie. Go again";
+		return message;
+	} else if (playerSelection === 'rock' && computerSelection === 'paper') {
+		computerWin('You lose. Paper beats rock');
+	} else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+		playerWin('You win. Rock beats scissors');
+	} else if (playerSelection === 'paper' && computerSelection === 'rock') {
+		playerWin('You win. Paper beats rock');
+	} else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+		computerWin('You lose. Scissors beats paper');
+	} else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+		computerWin('You lose. Rock beats scissors');
+	} else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+		playerWin('You win. Scissors beats paper');
+	} else {
+		message = 'Oops. Something went wrong';
+		return message;
+	}
+
+	console.log(
+		`${message}. Score: You ${playerScore}-${computerScore} The computer.`
+	);
+}
+
+// plays a game of best of five and prints final score
+function game() {
+	let counter = 0;
+	do {
+		console.log(playRound(userPlay(), computerPlay()));
+		if (playerScore === 3) {
+			console.log(
+				`You've won the game! Final Score: You ${playerScore}-${computerScore} The computer.`
+			);
+			counter = 1;
+		} else if (computerScore === 3) {
+			console.log(
+				`You've lost the game. Final Score: You ${playerScore}-${computerScore} The computer.`
+			);
+			counter = 1;
+		}
+	} while (counter < 1);
+}
+
+game();
